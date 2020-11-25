f28a15f1fcfa1df7a5b4d33d9e29b2d5c97f91ff
root <onipetheoderic@gmail.com>
2020-11-25 15:27:41 +0000

    initail commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,"261986980239-khme5q5p14vmkqhencg5ofom2tofua77.apps.googleusercontent.com","L8wiAksWLZG5ATTtkqZkiNOR"
+end
